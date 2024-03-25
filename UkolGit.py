27e991dbaa858fe3ca464960ecfa400aabe78717
unknown <bronislavdrimal@email.cz>
2024-03-25 10:23:10 +0100

    První popis ukolu

---
+++ UkolGit.py
@@ -0,0 +1,36 @@
+# Soubor vytvořený pro úkol a práci s git.
+#Úkol 1
+#Nainstalujte a nakonfigurujte Git na místním počítači.
+
+#Úkol 2
+#Vytvořte složku se dvěma podsložkami. Přidejte několik souborů do každá podsložka.
+
+#Úkol 3
+#Vytvořte úložiště v hlavní složce.
+
+#Úkol 4
+#Přidejte obsah dvou podsložek do indexu úložiště pomocí tohoto příkazu: git add.
+
+#Úkol 5
+#Vytvořte potvrzení na základě podsložek přidaných do indexu. Použijte tento příkaz: git commit.
+
+#Úkol 6
+#Změňte obsah jednoho ze souborů v podsložce a vytvořte potvrzení s novými daty.
+
+#Úkol 7
+#Vytvořte novou podsložku a vyplňte ji daty. Potom, vytvořte potvrzení s obsahem nové podsložky.
+
+#Úkol 8
+#Proveďte změny obsahu souboru v nové podsložce a uložte je (nevytvářejte nový odevzdání). Zrušit změny v místní kopii souboru pomocí tohoto příkazu: git checkout.
+
+#Úkol 9
+#Proveďte změny v obsahu nové podsložky a přidejte změny do indexu (nevytvářejte nový odevzdání). Zrušte změny v indexu pomocí tohoto příkazu: git reset.
+
+#Úkol 10
+#Proveďte změny obsahu souboru v nové podsložce, přidejte změny do indexu a vytvořte nové potvrzení. zrušení změny v posledním potvrzení vytvořením nového potvrzení pomocí tento příkaz: git revert.
+
+#Úkol 11
+#Odstraňte několik posledních odevzdání. Použijte tento příkaz: git reset s volbou --hard.
+
+#Úkol 12
+#Proveďte změny v posledním odevzdání bez vytvoření nového jeden. Použijte tento příkaz: git commit s klíčem --amend
